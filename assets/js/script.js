69eab6cb2dfe9254cf90d9cd43cf73072ed0316b
donna-chang <donnac.design@gmail.com>
2025-07-28 16:14:04 +0800

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,13 +6,14 @@ document.getElementById('hamburger').addEventListener('click', function () {
 
 // Scroll to top button
 const scrollTopBtn = document.getElementById("scrollTopBtn");
-window.onscroll = function () {
-  if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY > 300) {
     scrollTopBtn.style.display = "block";
   } else {
     scrollTopBtn.style.display = "none";
   }
-};
+});
 
 scrollTopBtn.addEventListener("click", function () {
   window.scrollTo({ top: 0, behavior: "smooth" });
